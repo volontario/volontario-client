03911a07a6fb3d5b382c5e340ecd82f17e8fed1a
Oliver Vartiainen <firoxer@gmail.com>
2015-11-22 22:09:45 +0200

    Fix routing

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('client/www'));
-app.use('/admin', express.static('admin/dist'));
+app.use('/admin', express.static('admin/app'));
 
 app.listen(process.env.PORT, function() {
   console.log('Server up');
