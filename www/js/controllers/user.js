2174d13b15e9104fe0c6b18bd41c03c339f86894
jpk24rent <jarkko@24rent.fi>
2015-11-25 17:07:03 +0200

    user as service & id set

--- www/js/controllers/user.js
+++ www/js/controllers/user.js
@@ -1,15 +1,10 @@
 angular.module('lg.controllers')
- .value('CORSURL', '//cors-anywhere.herokuapp.com/')
- .value('APIURL', 'https://volontario-server.herokuapp.com/')
-.controller('UserCtrl', function($scope,$rootScope,$state,$http, CORSURL, APIURL) {
 
-	$scope.user = {};
-	 
-	 $http.get(CORSURL+APIURL+'/users/5655c20b1f771103001ff9bd').then(function successCallBack(response){
-		$scope.user = response.data;
-	}, function errorCallBack(response){
+.controller('UserCtrl', function($scope,$rootScope,$state, User) {
 
-	})
+
+
+
 })
 
 
