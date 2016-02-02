c5be437e84eccd0ceacf7d12625f10d6374fa553
jpk24rent <jarkko@24rent.fi>
2016-02-02 19:20:32 +0200

    User profile remastered
    Add skills and preferences for further use

--- client/www/js/controllers/user.js
+++ client/www/js/controllers/user.js
@@ -2,9 +2,19 @@ angular.module('lg.controllers')
 
 .controller('UserCtrl', function($scope,$rootScope,$state, User) {
 
+    $scope.skill = [{skill: 'häsäys'},{skill:"EA1"}];
+
+	$scope.addSkill = function(skill){
+		$scope.skill.push({skill: skill});
+	};
+
+  	$scope.preference = [{preference: 'sosialisointi'},{preference:"magioinit"}];
+
+	$scope.addPreference = function(preference){
+		$scope.preference.push({preference: preference});
+	};
 
 
-
-})
+});
 
 
