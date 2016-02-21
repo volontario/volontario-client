89372e6ed61fe79f0020191c2c8d7064472b2d16
Oliver Vartiainen <firoxer@gmail.com>
2016-02-21 19:44:13 +0200

    Move $http requests to data-factory.js

--- client/www/js/controllers/userActivity.js
+++ client/www/js/controllers/userActivity.js
@@ -1,32 +1,22 @@
 angular.module('lg.controllers')
 
-.controller('UserActivityCtrl', function($scope,$rootScope, $state, $http, CORS_PROXY, API_ROOT, User) {
+.controller('UserActivityCtrl', function($scope,$rootScope, $state, dataFactory) {
 
     var userId = $scope.user.id;//"56a24593b42e9f03002b54b7";
-    var Auth = 'Basic dGVzdC50ZXN0QHRlc3QudGVzdDp0ZXN0';
 
 	var searchEvents = function(){
-    $http({
-         url: CORS_PROXY+API_ROOT+'users/'+userId+'/events',
-         method: 'GET',
-         headers: {'Authorization': Auth},
-         }).then(function successCallBack(response){
+
+    dataFactory.getUserEvents(userId)
+    .then(function successCallBack(response){
 		$scope.userEvents = response.data;
 		 }, function errorCallBack(response){
 
 	 });
   };
-    /* DELETE /events/:id/calendar
-	/* Deletes an item in an event calendar. Returns 204 on success.
-	/*	id 	ID of the item
-	*/
 
 	$scope.removeFromCalendar = function(eventId,uniqueId){
 		//save to localStorage and request from a service
-        var Auth = 'Basic dGVzdC50ZXN0QHRlc3QudGVzdDp0ZXN0';   
-        $http.delete(CORS_PROXY+API_ROOT+'events/'+eventId+'/calendar/'+uniqueId,
-            {headers: {'Authorization': Auth},
-            })
+        dataFactory.deleteEventCalendarItem(eventId, uniqueId)
           .success(function successCallBack(response){
             // add response from API
             console.log('removed');
