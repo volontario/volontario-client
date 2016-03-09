e5e838c1488f36d00c2ab193749fbd3f6fd08416
jpk24rent <jarkko@24rent.fi>
2016-03-09 19:00:51 +0200

    user tag & preferences placeholder

--- www/js/app.js
+++ www/js/app.js
@@ -153,5 +153,5 @@ angular.module('lg', [
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/login');
+  $urlRouterProvider.otherwise('/app/list');
 });
